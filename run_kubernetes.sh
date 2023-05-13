abad54c24d7f046a230cffcca5c012b7fbc2f73f
Fury <fiction.world18@gmail.com>
2023-05-13 12:02:59 +0700

    add kubernetes logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-dockerpath="nguyenanhduy032/project4:latest"
+dockerpath="nguyenanhduy032/project4"
 echo $dockerpath
-kubectl run predict --image=$dockerpath --port=80
+kubectl run predict --image=$dockerpath --port=80 --labels app=udafury
 kubectl get pod 
 kubectl port-forward predict 8000:80
